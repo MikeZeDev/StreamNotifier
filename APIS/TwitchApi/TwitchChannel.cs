3caa316f302a866d66a3d61ab92ff70aefdb97f9
MikeZeDev <turgoditapette@gmail.com>
2018-05-07 18:15:14 +0200

    TwitchAPI : Workaround for SSL/TLS issue

--- APIS/TwitchApi/TwitchChannel.cs
+++ APIS/TwitchApi/TwitchChannel.cs
@@ -37,8 +37,10 @@ namespace TwitchApi
 
 
             if (init)
-            { 
+            {
                 //download user icon
+                SecurityProtocolType backupprotocol = ServicePointManager.SecurityProtocol;
+
                 try
                 {
                     string filename = "channel_" + this.internalid + "_" + Path.GetFileName(new Uri(logo).AbsolutePath);
@@ -48,12 +50,19 @@ namespace TwitchApi
                     using (WebClient client = new WebClient())
                     {
                         File.Delete(this.localavatar);
+                       
+
+                        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
                         client.DownloadFile(this.logo, this.localavatar);
                         Helper.ScaleImageFile(this.localavatar,80,80);
                     }
 
                 }
-                catch { }
+                catch
+                {
+                }
+
+                 ServicePointManager.SecurityProtocol = backupprotocol;
 
             }
         }
